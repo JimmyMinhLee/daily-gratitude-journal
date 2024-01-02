18879dccbda486eb0aebed0513d5a4b70e2c601e
Jimmy Lee <jimmyminhlee@gmail.com>
2024-01-01 21:48:14 -0800

    wip: update description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,23 +8,28 @@ export default function About() {
       <Animate delay={0.2}>
         <Heading variant="section-title">why?</Heading>
         <Paragraph>
-          the act of being reminded & confronting your gratitudes is connected
-          to improved mental health in the face of stress. i could not find a
-          place online where you're able to keep track of these gratitudes and
-          to journal them. the intention is to provide a platform for doing this
-          while you're at your computer, or on your phone.
+          expressing gratitude is fun. taking a moment out of your day to pause
+          and collect your thoughts on everything that makes you happy or joyful
+          is a great way to slow down your pace and appreciate all the little
+          things that life has to offer. unfortunately, there isn't a place
+          online for you to do so, and while writing things down on pen and
+          paper will always be available, it can be cumbersome. this website
+          aims to be the one place for you to be able to document your
+          gratitudes, whether that's while you're on the go and want to quickly
+          jot something down with your phone, or on a friday evening where
+          you're at your desk & scrolling on your laptop.
         </Paragraph>
       </Animate>
 
       <Animate delay={0.3}>
         <Heading variant="section-title">what's to come?</Heading>
         <Paragraph>
-          i have a lot of ideas to implement on this website during my free
-          time. currently, it just provides basic functionality for creating
-          these gratitudes and plainly viewing them. some improvements are the
-          ability to #tag certain gratitudes, and providing a search filter for
-          your gratitudes. it'd be fun to implement some scoring per user as
-          well.
+          while i have a lot of ideas to bring to life on this little pocket of
+          the internet, i have a laundry list of other things that i'd like to
+          do first. primarily, this was just a silly project to better
+          understand the web technologies used to build it. if the usage is
+          there, though, i'd love to pick it up a little more seriously and to
+          bring it to complete maturity.
         </Paragraph>
       </Animate>
     </Stack>
