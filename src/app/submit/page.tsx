f3d6b4921575323e2a689c29448ec46a79f8182d
Jimmy Lee <jimmyminhlee@gmail.com>
2024-01-01 17:24:32 -0800

    wip: submit works

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -2,18 +2,26 @@
 
 import { Button, Stack, Textarea } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { sql } from "@vercel/postgres";
 import React from "react";
 
 export default function SubmitGratitude() {
   const [gratitude, setGratitude] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
   const router = useRouter();
-  const handleInputChange = (e: any) => {
-    setGratitude(e.target.value);
-  };
 
-  const handleClick = () => {
-    router.push("/gratitudes");
+  const submitData = async (e: React.SyntheticEvent) => {
+    try {
+      setLoading(true);
+      const body = { title: "hello", content: gratitude };
+      await fetch("/api/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      router.push("/gratitudes");
+    } catch (error) {
+      console.error(`Error occured while pushing gratitude post: ${error}`);
+    }
   };
 
   return (
@@ -21,10 +29,16 @@ export default function SubmitGratitude() {
       <Stack direction="column">
         <Textarea
           value={gratitude}
-          onChange={handleInputChange}
+          onChange={(e: any) => {
+            setGratitude(e.target.value);
+          }}
           placeholder="a nice warm coffee..."
         />
-        <Button onClick={handleClick}>submit</Button>
+        {isLoading ? (
+          <Button isLoading />
+        ) : (
+          <Button onClick={submitData}>submit</Button>
+        )}
       </Stack>
     </>
   );
